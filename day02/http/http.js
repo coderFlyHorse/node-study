3c3399efd42e4c53213c0b81a117b55ce20b7d5d
bArter <1285434016@qq.com>
2023-05-17 15:01:43 +0800

    笔记

---
+++ day02/http/http.js
@@ -0,0 +1,26 @@
+// http 协议 全称 超文本传输协议
+
+// http协议是 基于 tcp/ip协议的 浏览器与服务器之间的通信协议
+
+// http 报文 浏览器发送给服务器的报文叫做请求报文，服务器返回给浏览器的叫做响应报文 可以使用 fiddler 抓包查看
+
+// URL Uniform Resource Locator 统一资源定位符
+
+// https://www.bilibili.com/video/BV1gM411W7ex?p=37&spm_id_from=pageDriver&vd_source=f7e26065672c81e7797abfb85898fc8d
+// 协议  域名（会解析为IP地址）  参数（资源路径）
+
+// 请求头 Request Header
+// 请求体 Request Body Json 格式
+
+
+// 响应行 HTTP/1.1 200 OK
+// 状态码 1XX 响应信息
+// 状态码 2XX 成功响应
+// 状态码 3XX 重定向
+// 状态码 4XX 客户端错误响应
+// 状态码 5XX 服务端错误响应
+// 响应头  Response Header
+
+// 响应体  Response Body  html css js img video json
+
+// ip设备通信的基础 数字标识 
