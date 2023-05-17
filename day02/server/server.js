421d83f922aabe358730416e6170ae75850999a8
bArter <1285434016@qq.com>
2023-05-17 15:25:33 +0800

    创建http服务

---
+++ day02/server/server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+// request 请求 response 响应
+const server = http.createServer((request, response) => {
+
+    // 设置响应体 以及 告诉浏览器 解码的格式
+    response.setHeader("content-type", "text/html; charset=utf-8");
+    response.end("你好 HTTP Server")
+})
+
+server.listen(9000, () => {
+    console.log("服务已经启动");
+})
